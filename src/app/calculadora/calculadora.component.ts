20f7585288e1390f16b0696e554f0fbdd9220397
Leon Santana <leon.santana854@gmail.com>
2025-04-24 20:57:43 -0300

    feat(calculadora): adiciona funcionalidade de soma entre dois números

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,11 +1,23 @@
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-calculadora',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.scss'
 })
 export class CalculadoraComponent {
 
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+
+  calcularResultado() {
+    console.log("Chamando o método para calcular o resultado!");
+    console.log(`Numero 1: ${this.numero1}`);
+    console.log(`Numero 2: ${this.numero2}`);
+    this.resultado = this.numero1 + this.numero2;
+  }
 }
